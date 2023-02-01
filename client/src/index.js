f9282647d8c74a8bcde50e52f1e554d8586c0771
Joaquin <joaquinpalacio2311@gmail.com>
2023-02-01 19:18:36 -0300

    add components and other details

--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
+import { Provider } from 'react-redux';
+import { store } from './store/index';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
-
