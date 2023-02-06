d44ab01a9ae3b4c16a8530d2f6a80b3c6023a7e6
Joaquin <joaquinpalacio2311@gmail.com>
2023-02-06 10:19:21 -0300

    app retoques

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import { /* Route, */ Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-
+        <Route></Route>
       </Routes>
     </div>
   );
