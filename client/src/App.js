40ca2502e0b2b1b7749e2df724a60e17e2a74b04
Joaquin <joaquinpalacio2311@gmail.com>
2023-02-08 12:08:34 -0300

    fix route /home

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <div className='App'>
         <Routes>
           <Route exact path='/' element={ <LandingPage /> } />
-          <Route exact path='/dogs' element={ <Home /> } />
+          <Route exact path='/home' element={ <Home /> } />
         </Routes>
       </div>
     </BrowserRouter>
