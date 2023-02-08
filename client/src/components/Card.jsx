e4b9bae721aa4f47eb3c9bd89377114e89b96c7e
Joaquin <joaquinpalacio2311@gmail.com>
2023-02-08 11:13:28 -0300

    Add Card

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React from 'react';
 
-export const Card = () => {
+export const Card = ({ name, image, temperaments }) => {
   return (
-    <div>Card</div>
-  )
-}
+    <div>
+      <h1>{name}</h1>
+      <img src={image} alt='imagen_perro' />
+      <h2>Temperamentos:</h2>
+      <h3>
+        {function (temperaments) {
+          if (typeof temperaments === 'string') return temperaments;
+          if (Array.isArray(temperaments)) {
+            let temps = temperaments.map((e) => e.name);
+            return temps.join(', ');
+          }
+        }(temperaments)}
+      </h3>
+    </div>
+  );
+};
